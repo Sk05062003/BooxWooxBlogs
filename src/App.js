ac3fcbbbdbd8e091d9f24ef951cf070aa0f1257b
Dinesh Chukkala <dineshchukkala169@gmail.com>
2021-07-29 17:06:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
-// import './App.css';
-// import '@icon/icofont/icofont.css'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'aos/dist/aos.css';
-// import 'owl.carousel/dist/assets/owl.carousel.min.css';
-// import 'line-awesome/dist/line-awesome/css/line-awesome.min.css'
-// import './style.css'
-import AOS from 'aos';
-import 'font-awesome/css/font-awesome.css'
-import BlogPage from "./components/blog-page";
-AOS.init();
+import './App.css'
+
 function App() {
-  return (
-    <BlogPage />
-  );
+  return <div>Hello World</div>
 }
 
-export default App;
+export default App
