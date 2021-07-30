74d39b0cd871f868cbbf7e7efc4c7af08d961057
Komachian <adiljamal221101@gmail.com>
2021-07-30 13:50:19 +0530

    Navbar and Miscellaneous Components

---
+++ src/components/nav.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react'
+import './nav.css'
+import logo from '../bwlogo.svg'
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    const[click, setClick] = useState(false);
+
+    const iconChange = () => setClick(prevClick => !prevClick);
+    const closeSideMenu = () => setClick(false);
+
+    const checkSideMenuSize = () => {
+        if(window.innerWidth >= 800) {
+            closeSideMenu();
+        }
+    }
+    
+    window.addEventListener('resize', checkSideMenuSize);
+
+    return (
+        <>
+            <nav className = "navbar">
+                <div className = "logo" onClick = {closeSideMenu}>
+                    <Link to = "/"><img src = {logo} /></Link>
+                </div>
+
+                <div className = "menu-bar-icon" onClick = {iconChange}>
+                    <i className = {click ? "fa fa-times" : "fa fa-bars"} />
+                </div>
+
+                <div className = "nav-row">
+                    <Link to = "/features" className = "nav-link" id = "feat">
+                        Features
+                    </Link>
+                    <Link to = "/blogs" className = "nav-link" id = "blog">
+                        Blogs
+                    </Link>
+                    <Link to = "/downloads" className = "nav-link" id = "down">
+                        Downloads
+                    </Link>
+                    <Link to = "/login" className = "nav-link" id = "log-text">
+                        Login
+                    </Link>
+                </div>
+            </nav>
+            <div id = "side-menu-container">
+            <ul className = {click ? "side-menu-active" : "side-menu"}>
+                <li className = "side-menu-item">
+                    <Link to = "/features" className = "nav-link-side" onClick = {closeSideMenu}>
+                        Features
+                    </Link>
+                </li>
+                <li className = "side-menu-item">
+                    <Link to = "/blogs" className = "nav-link-side" id = "blog-side" onClick = {closeSideMenu}>
+                        Blogs
+                    </Link>
+                </li>
+                <li className = "side-menu-item">
+                    <Link to = "/downloads" className = "nav-link-side" onClick = {closeSideMenu}>
+                        Downloads
+                    </Link>
+                </li>
+                <li className = "side-menu-item">
+                    <Link to = "/login" className = "nav-link-side" id = "log-side" onClick = {closeSideMenu}>
+                        Login
+                    </Link>
+                </li>
+            </ul>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
