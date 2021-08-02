617bbbfeabc48e679e2f3498f4b8dec6c2c9205d
Komachian <adiljamal221101@gmail.com>
2021-08-02 12:15:31 +0530

    Blogs Page Created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import Navbar from './components/Global/nav'
 import './App.css'
 import Home from './components/Home/Home'
+import BlogsPage from './components/pages/blogs'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <Navbar />
+      {/* <BlogsPage /> */}
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/blogs' component={BlogsPage} />
       </Switch>
     </Router>
   )
